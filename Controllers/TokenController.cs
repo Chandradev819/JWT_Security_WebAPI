fa610d7d4d8633b939500aefd6b0b7647103f5f4
Chandradev <chandradev819@hotmail.com>
2021-05-08 20:48:55 +0530

    Add project files.

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,26 @@
+﻿using AuthWebAPI_Demo.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthWebAPI_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public string GetRandomToken()
+        {
+            var jwt = new JwtServices(_config);
+            var token = jwt.GenerateSecurityToken("fake@email.com");
+            return token;
+        }
+    }
+}
